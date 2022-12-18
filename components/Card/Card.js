648f7d5fbc12e72b267d756aef3c270414d829c7
sajib925 <sajibahmed1004@gmail.com>
2022-12-18 11:48:55 +0600

    some updated

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -3,11 +3,12 @@ import React from 'react'
 import Paragraph from '../Paragraph/Paragraph';
 import Title from '../Title/Title';
 import StyledCard from './StyledCard'
+import  Link  from 'next/link';
 
 const Card = ({subTitle, title, img, path="#", className}) => {
   return (
     <StyledCard className={className}>
-        <a className='card__link' href={path}></a>
+        <Link className='card__link' href={path}></Link>
           <Title tagName="h3" className="title">
             {title}
           </Title>
